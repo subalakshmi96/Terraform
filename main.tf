01dc3838857b6532bb9031a9e60382d0c57adc5f
subalakshmi96 <subalakshmikandhasamy96@gmail.com>
2026-07-06 15:56:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+resource "aws_instance" "web_server" {
+  ami           = "ami-002b685502867073a"
+  instance_type = "t3.micro"
 }
