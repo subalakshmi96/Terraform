627660db7a392eef33b1be8919f3d5450649fa4c
subalakshmikandhasamy <subalakshmikandhasamy96@gmail.com>
2026-07-09 16:49:12 +0530

    Update output.tf

--- modules/ec2_instance/output.tf
+++ modules/ec2_instance/output.tf
@@ -1,11 +1,3 @@
-variable "ami_value" {
-    description = "value for the ami"
-}
-
-variable "instance_type_value" {
-    description = "value for instance_type"
-}
-
-variable "subnet_id_value" {
-    description = "value for the subnet_id"
+output "public-ip-address" {
+  value = aws_instance.example.public_ip
 }
